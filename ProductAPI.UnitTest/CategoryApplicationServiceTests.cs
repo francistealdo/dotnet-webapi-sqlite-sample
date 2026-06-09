da3b9ce63449ce01e3e9badfbcce8b54b586bd82
Francis Tealdo <francistealdo@gmail.com>
2026-06-09 20:47:49 -0300

    Add unit tests for product and category application services

---
+++ ProductAPI.UnitTest/CategoryApplicationServiceTests.cs
@@ -0,0 +1,146 @@
+﻿using FluentAssertions;
+using Moq;
+using ProductAPI.Application;
+using ProductAPI.Application.Dto;
+using ProductAPI.Application.Interface.Mapper;
+using ProductAPI.Domain.Core.Interface.Service;
+using ProductAPI.Domain.Entity;
+
+namespace ProductAPI.UnitTest.Application;
+
+public class CategoryApplicationServiceTests
+{
+    private readonly Mock<ICategoryService> _categoryServiceMock;
+    private readonly Mock<ICategoryMapper> _categoryMapperMock;
+    private readonly CategoryApplicationService _service;
+
+    public CategoryApplicationServiceTests()
+    {
+        _categoryServiceMock = new Mock<ICategoryService>();
+        _categoryMapperMock = new Mock<ICategoryMapper>();
+
+        _service = new CategoryApplicationService(
+            _categoryServiceMock.Object,
+            _categoryMapperMock.Object);
+    }
+
+    [Fact]
+    public void GetById_ShouldReturnCategoryDto_WhenCategoryExists()
+    {
+        var category = new Category { Id = 1, Name = "Electronics" };
+        var categoryDto = new CategoryDto { Id = 1, Name = "Electronics" };
+
+        _categoryServiceMock.Setup(x => x.GetById(1))
+            .Returns(category);
+
+        _categoryMapperMock.Setup(x => x.MapperEntityToDto(category))
+            .Returns(categoryDto);
+
+        var result = _service.GetById(1);
+
+        result.Should().NotBeNull();
+        result!.Id.Should().Be(1);
+        result.Name.Should().Be("Electronics");
+    }
+
+    [Fact]
+    public void GetById_ShouldReturnNull_WhenCategoryDoesNotExist()
+    {
+        _categoryServiceMock.Setup(x => x.GetById(99))
+            .Returns((Category?)null);
+
+        var result = _service.GetById(99);
+
+        result.Should().BeNull();
+    }
+
+    [Fact]
+    public void GetAll_ShouldReturnCategoryDtoCollection()
+    {
+        var categories = new List<Category>
+        {
+            new Category { Id = 1, Name = "Electronics" },
+            new Category { Id = 2, Name = "Books" }
+        };
+
+        var categoryDtos = new List<CategoryDto>
+        {
+            new CategoryDto { Id = 1, Name = "Electronics" },
+            new CategoryDto { Id = 2, Name = "Books" }
+        };
+
+        _categoryServiceMock.Setup(x => x.GetAll())
+            .Returns(categories);
+
+        _categoryMapperMock.Setup(x => x.MapperListEntityToDto(categories))
+            .Returns(categoryDtos);
+
+        var result = _service.GetAll();
+
+        result.Should().NotBeNull();
+        result.Should().HaveCount(2);
+        result.First().Name.Should().Be("Electronics");
+    }
+
+    [Fact]
+    public void Add_ShouldReturnCreatedCategoryDto()
+    {
+        var categoryDto = new CategoryDto { Name = "Games" };
+        var category = new Category { Name = "Games" };
+        var createdCategory = new Category { Id = 1, Name = "Games" };
+        var createdCategoryDto = new CategoryDto { Id = 1, Name = "Games" };
+
+        _categoryMapperMock.Setup(x => x.MapperDtoToEntity(categoryDto))
+            .Returns(category);
+
+        _categoryServiceMock.Setup(x => x.Add(category))
+            .Returns(createdCategory);
+
+        _categoryMapperMock.Setup(x => x.MapperEntityToDto(createdCategory))
+            .Returns(createdCategoryDto);
+
+        var result = _service.Add(categoryDto);
+
+        result.Should().NotBeNull();
+        result.Id.Should().Be(1);
+        result.Name.Should().Be("Games");
+
+        _categoryServiceMock.Verify(x => x.Add(category), Times.Once);
+    }
+
+    [Fact]
+    public void Update_ShouldReturnUpdatedCategoryDto()
+    {
+        var categoryDto = new CategoryDto { Id = 1, Name = "Updated Category" };
+        var category = new Category { Id = 1, Name = "Updated Category" };
+
+        _categoryMapperMock.Setup(x => x.MapperDtoToEntity(categoryDto))
+            .Returns(category);
+
+        _categoryServiceMock.Setup(x => x.Update(category))
+            .Returns(category);
+
+        _categoryMapperMock.Setup(x => x.MapperEntityToDto(category))
+            .Returns(categoryDto);
+
+        var result = _service.Update(categoryDto);
+
+        result.Should().NotBeNull();
+        result.Id.Should().Be(1);
+        result.Name.Should().Be("Updated Category");
+
+        _categoryServiceMock.Verify(x => x.Update(category), Times.Once);
+    }
+
+    [Fact]
+    public void Delete_ShouldCallCategoryServiceDelete()
+    {
+        var categoryId = 1;
+
+        _service.Delete(categoryId);
+
+        _categoryServiceMock.Verify(
+            x => x.Delete(categoryId),
+            Times.Once);
+    }
+}
---
+++ ProductAPI.UnitTest/ProductApplicationServiceTests.cs
@@ -0,0 +1,228 @@
+﻿using FluentAssertions;
+using Moq;
+using ProductAPI.Application;
+using ProductAPI.Application.Dto;
+using ProductAPI.Application.Interface.Mapper;
+using ProductAPI.Domain.Core.Interface.Service;
+using ProductAPI.Domain.Entity;
+
+namespace ProductAPI.UnitTest
+{
+    public class ProductApplicationServiceTests
+    {
+        private readonly Mock<IProductService> _productServiceMock;
+        private readonly Mock<IProductMapper> _productMapperMock;
+        private readonly ProductApplicationService _service;
+
+        public ProductApplicationServiceTests()
+        {
+            _productServiceMock = new Mock<IProductService>();
+            _productMapperMock = new Mock<IProductMapper>();
+
+            _service = new ProductApplicationService(
+                _productServiceMock.Object,
+                _productMapperMock.Object);
+        }
+
+        [Fact]
+        public void GetById_ShouldReturnProductDto_WhenProductExists()
+        {
+            // Arrange
+
+            var product = new Product
+            {
+                Id = 1,
+                Name = "Notebook"
+            };
+
+            var productDto = new ProductDto
+            {
+                Id = 1,
+                Name = "Notebook"
+            };
+
+            var productServiceMock =
+                new Mock<IProductService>();
+
+            var mapperMock =
+                new Mock<IProductMapper>();
+
+            productServiceMock
+                .Setup(x => x.GetById(1))
+                .Returns(product);
+
+            mapperMock
+                .Setup(x => x.MapperEntityToDto(product))
+                .Returns(productDto);
+
+            var applicationService =
+                new ProductApplicationService(
+                    productServiceMock.Object,
+                    mapperMock.Object);
+
+            // Act
+
+            var result = applicationService.GetById(1);
+
+            // Assert
+
+            result.Should().NotBeNull();
+            result.Id.Should().Be(1);
+            result.Name.Should().Be("Notebook");
+        }
+
+        [Fact]
+        public void GetById_ShouldReturnNull_WhenProductDoesNotExist()
+        {
+            _productServiceMock
+                .Setup(x => x.GetById(99))
+                .Returns((Product?)null);
+
+            var result = _service.GetById(99);
+
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public void GetAll_ShouldReturnProductDtoCollection()
+        {
+            var products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Notebook" },
+            new Product { Id = 2, Name = "Mouse" }
+        };
+
+            var productDtos = new List<ProductDto>
+        {
+            new ProductDto { Id = 1, Name = "Notebook" },
+            new ProductDto { Id = 2, Name = "Mouse" }
+        };
+
+            _productServiceMock
+                .Setup(x => x.GetAll())
+                .Returns(products);
+
+            _productMapperMock
+                .Setup(x => x.MapperListEntityToDto(products))
+                .Returns(productDtos);
+
+            var result = _service.GetAll();
+
+            result.Should().NotBeNull();
+            result.Should().HaveCount(2);
+            result.First().Name.Should().Be("Notebook");
+        }
+
+        [Fact]
+        public void Add_ShouldReturnCreatedProductDto()
+        {
+            var productDto = new ProductDto
+            {
+                Name = "Keyboard",
+                SKU = "KEY-001",
+                Price = 150,
+                StockQuantity = 10
+            };
+
+            var product = new Product
+            {
+                Name = "Keyboard",
+                SKU = "KEY-001",
+                Price = 150,
+                StockQuantity = 10
+            };
+
+            var createdProduct = new Product
+            {
+                Id = 1,
+                Name = "Keyboard",
+                SKU = "KEY-001",
+                Price = 150,
+                StockQuantity = 10
+            };
+
+            var createdProductDto = new ProductDto
+            {
+                Id = 1,
+                Name = "Keyboard",
+                SKU = "KEY-001",
+                Price = 150,
+                StockQuantity = 10
+            };
+
+            _productMapperMock
+                .Setup(x => x.MapperDtoToEntity(productDto))
+                .Returns(product);
+
+            _productServiceMock
+                .Setup(x => x.Add(product))
+                .Returns(createdProduct);
+
+            _productMapperMock
+                .Setup(x => x.MapperEntityToDto(createdProduct))
+                .Returns(createdProductDto);
+
+            var result = _service.Add(productDto);
+
+            result.Should().NotBeNull();
+            result.Id.Should().Be(1);
+            result.Name.Should().Be("Keyboard");
+
+            _productServiceMock.Verify(x => x.Add(product), Times.Once);
+        }
+
+        [Fact]
+        public void Update_ShouldReturnUpdatedProductDto()
+        {
+            var productDto = new ProductDto
+            {
+                Id = 1,
+                Name = "Updated Notebook",
+                SKU = "NOTE-001",
+                Price = 4000,
+                StockQuantity = 5
+            };
+
+            var product = new Product
+            {
+                Id = 1,
+                Name = "Updated Notebook",
+                SKU = "NOTE-001",
+                Price = 4000,
+                StockQuantity = 5
+            };
+
+            _productMapperMock
+                .Setup(x => x.MapperDtoToEntity(productDto))
+                .Returns(product);
+
+            _productServiceMock
+                .Setup(x => x.Update(product))
+                .Returns(product);
+
+            _productMapperMock
+                .Setup(x => x.MapperEntityToDto(product))
+                .Returns(productDto);
+
+            var result = _service.Update(productDto);
+
+            result.Should().NotBeNull();
+            result.Id.Should().Be(1);
+            result.Name.Should().Be("Updated Notebook");
+
+            _productServiceMock.Verify(x => x.Update(product), Times.Once);
+        }
+
+        [Fact]
+        public void Delete_ShouldCallProductServiceDelete()
+        {
+            var productId = 1;
+
+            _service.Delete(productId);
+
+            _productServiceMock.Verify(
+                x => x.Delete(productId),
+                Times.Once);
+        }
+    }
+}
